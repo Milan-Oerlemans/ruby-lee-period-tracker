3b4fcb80b23f05a2e2a02529f5ebb2ebebf5014e
Milan <milan@oerlemans.co.za>
2026-05-12 17:23:05 +0200

    deploy fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useContext, useEffect, useState } from "react";
-import { Redirect, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import {
   IonApp,
   IonLabel,
